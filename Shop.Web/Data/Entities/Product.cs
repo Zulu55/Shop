47f733a4c3dd983553ffbf9508ceea085e8d07a5
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-02-08 19:26:25 -0500

    Add products controller

---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace Shop.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain a maximum of {1} characters")]
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime? LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime? LastSale { get; set; }
+
+        [Display(Name = "Is Availabe?")]
+        public bool IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
