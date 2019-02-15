998714a3de33f69914f110f61adce366be9bea93
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-02-15 08:58:09 -0500

    Add anotations to Country

--- Shop.Web/Data/Entities/Country.cs
+++ Shop.Web/Data/Entities/Country.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Shop.Web.Data.Entities
+﻿namespace Shop.Web.Data.Entities
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Country : IEntity
     {
-        public int Id { get ; set ; }
+        public int Id { get; set; }
 
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        [Required]
         public string Name { get; set; }
     }
 }
